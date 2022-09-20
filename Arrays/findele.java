a952d5cdb40bc6c62e25219497f911a7573bacf0
RajneeshDevSingh <singhrajneeshdev@gmail.com>
2022-09-21 00:56:10 +0530

    2

--- Arrays/findele.java
+++ Arrays/findele.java
@@ -2,29 +2,33 @@ package Arrays;
 
 import java.io.*;
 
-public class findele {
+public class findele 
+{   
+    public static void main(String[] args)
+    {
+        int arr[] = {1,2,3,4,5,6,7,8,9};
+
+        System.out.println(FindEle(arr, 5));
+    }
+
+    public static int FindEle(int arr[] , int n)
+    {
+        int idx = 0;
+
+        for(int i = 0; i<arr.length;i++)
+        {
+            if(arr[i] == n)
+            {
+                idx = i;
+            }
+
+        }
+
+
+        return idx;
+    }
 
 	
-	static String countMaxChoco(String s1 , String s2)
-	{
-		
-		
-		String s3=s1.concat(s2);
-		return s3;
-	}
-	
-
-	public static void main (String[] args)
-	{
-		
-		 String s1="Sachin";  
-        String s2="Tendulkar";  
-          
-		
-		
-		System.out.println(
-		countMaxChoco(s1 , s2));
-	}
 }
 
 
