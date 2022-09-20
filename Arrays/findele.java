cb84e49d2aae88e52276cd7cc9913c446909f327
RajneeshDevSingh <singhrajneeshdev@gmail.com>
2022-09-21 00:46:39 +0530

    first commit

---
+++ Arrays/findele.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+import java.io.*;
+
+public class findele {
+
+	
+	static String countMaxChoco(String s1 , String s2)
+	{
+		
+		
+		String s3=s1.concat(s2);
+		return s3;
+	}
+	
+
+	public static void main (String[] args)
+	{
+		
+		 String s1="Sachin";  
+        String s2="Tendulkar";  
+          
+		
+		
+		System.out.println(
+		countMaxChoco(s1 , s2));
+	}
+}
+
+
